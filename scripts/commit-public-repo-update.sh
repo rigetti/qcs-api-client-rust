ff0a3e1aeaf622edf203d2aab9a49876c1d1a4c4
Dylan Anthony <danthony@rigetti.com>
2022-08-08 10:57:33 -0600

    ci: Setup CI Release job

--- scripts/commit-public-repo-update.sh
+++ scripts/commit-public-repo-update.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 COMMIT_MESSAGE="${1:?"Must specify a commit message"}"
-WORKDIR=$(mktemp -d -p /tmp)
+WORKDIR=$(mktemp -d -p "$(pwd)")
 
 if [[ ! "$WORKDIR" || ! -d "$WORKDIR" ]]; then
   echo "Could not create temp dir"
@@ -21,18 +21,18 @@ trap cleanup EXIT
 git clone "https://$GITHUB_USER_NAME:$GITHUB_TOKEN@github.com/rigetti/qcs-api-client-rust.git" "$WORKDIR"
 
 # Delete all files except the .git/ directory.
-git rm -r -- "$WORKDIR/*"
-# Copy all files, including hidden files, from the public directory to the public Github repo.
-cp -rfT ../public "$WORKDIR"
-
 cd "$WORKDIR"
+git rm -r -- *
+# Copy all files, including hidden files, from the public directory to the public Github repo.
+cp -rfT ../public .
+
 # We cannot update Github workflows using a personal access token, so ignore changes to the following directory.
-git reset -- .github/workflows/publish.yaml
-git checkout -- .github/workflows/publish.yaml
+git reset -- .github/workflows/release.yml
+git checkout -- .github/workflows/release.yml
 
 git add --all
 # Print the changes to be committed to the public Github repo.
 git status
 
 git commit -m "$COMMIT_MESSAGE"
-
+git push
