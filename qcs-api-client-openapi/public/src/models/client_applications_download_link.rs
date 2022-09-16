7774aca985a73c9d64ca874476d8b6bd47de93df
Michael Bryant <mbryant@rigetti.com>
2022-09-15 17:03:38 -0700

    chore(openapi): change copyright year on header to 2022

--- qcs-api-client-openapi/public/src/models/client_applications_download_link.rs
+++ qcs-api-client-openapi/public/src/models/client_applications_download_link.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 Rigetti Computing
+// Copyright 2022 Rigetti Computing
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
